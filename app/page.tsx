7fcbc87d8398c2777c9d320ec669e6fb0bb506b0
sheraz4196 <sheraztoor321@gmail.com>
2024-07-23 11:28:44 +0500

    feat: services section build

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Services from "@/components/homepage/services";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Services />
+    </>
+  );
 }
---
+++ components/homepage/services.tsx
@@ -0,0 +1,8 @@
+export default function Services() {
+  return (
+    <section className="bg-dark-foreground text-dark p-12 flex flex-col items-center">
+      <h2 className="text-2xl font-bold">What Can I do for you?</h2>
+      <div className="p-12"></div>
+    </section>
+  );
+}
