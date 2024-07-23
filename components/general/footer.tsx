20f68e182b3862a6b3d375a98215da0c4ad52544
sheraz4196 <sheraztoor321@gmail.com>
2024-07-23 10:39:40 +0500

    feat: svg logo component

---
+++ components/general/footer.tsx
@@ -0,0 +1,9 @@
+import LogoSvg from "../svgs/logo-svg";
+
+export default function Footer() {
+  return (
+    <footer className="py-5 px-12 bg-dark text-dark-foreground flex items-center justify-between">
+      <LogoSvg />
+    </footer>
+  );
+}
