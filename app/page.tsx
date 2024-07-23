542113de9427d32fc91209a507784ee7634d51de
sheraz4196 <sheraztoor321@gmail.com>
2024-07-23 11:56:12 +0500

    feat: pricing section initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Pricing from "@/components/homepage/pricing";
 import Services from "@/components/homepage/services";
 
 export default function Home() {
   return (
     <>
       <Services />
+      <Pricing />
     </>
   );
 }
---
+++ components/homepage/pricing.tsx
@@ -0,0 +1,8 @@
+export default function Pricing() {
+  return (
+    <section className="p-12 bg-dark-foreground">
+      <h2 className="text-2xl font-bold">What Can I do for you?</h2>
+      <div className="p-12"></div>
+    </section>
+  );
+}
