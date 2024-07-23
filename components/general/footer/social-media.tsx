dc186008808354d80c945247e6eeb2f4bef34952
sheraz4196 <sheraztoor321@gmail.com>
2024-07-23 11:17:29 +0500

    feat: social media content

--- components/general/footer/social-media.tsx
+++ components/general/footer/social-media.tsx
@@ -2,11 +2,12 @@ import BeSvg from "@/components/svgs/logos/be-svg";
 import FacebookSvg from "@/components/svgs/logos/facebook-svg";
 import InstaSvg from "@/components/svgs/logos/insta-svg";
 import TwitterSvg from "@/components/svgs/logos/twitter-svg";
+import { Phone } from "lucide-react";
 import Link from "next/link";
 
 export default function SocialMedia() {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <div className="p-2.5 flex items-center gap-2.5">
         <Link href={"#"}>
           <TwitterSvg />
@@ -23,6 +24,15 @@ export default function SocialMedia() {
         <Link href={"#"}>
           <FacebookSvg />
           <span className="sr-only">Facebook Account</span>
+        </Link>
+      </div>
+      <div className="p-2.5">
+        <Link
+          href={"#"}
+          className="flex items-center gap-1.5 text-sm font-bold"
+        >
+          <Phone />
+          <span>+92 123 456 1234</span>
         </Link>
       </div>
     </div>
