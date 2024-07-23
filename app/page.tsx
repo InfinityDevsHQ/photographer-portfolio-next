b64ffd7862542dc06bb9bfd051369a678f71f5ad
Danish <danishasifdev@gmail.com>
2024-07-23 16:10:04 +0500

    feat: add navbar and hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Hero from "@/components/homepage/hero";
+import Navbar from "@/components/homepage/navbar";
 import Pricing from "@/components/homepage/pricing";
 import Services from "@/components/homepage/services";
 import About from "@/components/svgs/about";
@@ -5,6 +7,11 @@ import About from "@/components/svgs/about";
 export default function Home() {
   return (
     <>
+      <div className="relative bg-[url('/hero-img.jpeg')] text-white bg-cover bg-left md:bg-center xl:bg-cover bg-no-repeat min-h-[600px]">
+        <div className="absolute inset-0 -z-10 bg-black opacity-50"></div>
+        <Navbar />
+        <Hero />
+      </div>
       <Services />
       <Pricing />
       <About />
