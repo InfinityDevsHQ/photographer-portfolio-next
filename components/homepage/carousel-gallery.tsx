57c9853a5a1a4a15b622e595985e26118feba7a1
sheraz4196 <sheraztoor321@gmail.com>
2024-07-24 10:21:26 +0500

    feat: carousel main layout

--- components/homepage/carousel-gallery.tsx
+++ components/homepage/carousel-gallery.tsx
@@ -1,3 +1,40 @@
+import Image from "next/image";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+
 export default function CarouselGallery() {
-  return <section></section>;
+  return (
+    <section>
+      <Carousel>
+        <CarouselContent className="gap-0">
+          <CarouselItem className="basis-1/3 pl-0">
+            <Image
+              src={"/assets/gallery/gallery-1.png"}
+              width={226}
+              height={600}
+              alt="Art"
+              className="w-full"
+            />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3 !pl-0">
+            <Image
+              src={"/assets/gallery/gallery-2.png"}
+              width={226}
+              height={600}
+              alt="Art"
+              className="w-full"
+            />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3 !pl-0">
+            <Image
+              src={"/assets/gallery/gallery-3.png"}
+              width={226}
+              height={600}
+              alt="Art"
+              className="w-full"
+            />
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+    </section>
+  );
 }
